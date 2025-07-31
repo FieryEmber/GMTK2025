cbcb313c30b1ea2ed9fcc6100c5b0f2da1858c2b
Hayden Galyean <haydengalyeanbiz@gmail.com>
2025-07-31 11:46:53 -0400

    working on fixing jittery movement

--- project-loop/Scenes/mobius.gd
+++ project-loop/Scenes/mobius.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-@export var SPEED = 150
-@export var JUMP_VELOCITY = -300
+@export var SPEED = 180
+@export var JUMP_VELOCITY = -400
 
 const GRAVITY = 900
 const JUMP_CUT_MULTIPLIER = 0.2
@@ -20,19 +20,21 @@ func _physics_process(delta):
 
 	# Horizontal movement
 	if Input.is_action_pressed("left"):
-		velocity.x -= SPEED * delta * 1000
+		velocity.x -= SPEED
 		is_moving = true
 		anim.flip_h = true
 	elif Input.is_action_pressed("right"):
-		velocity.x += SPEED * delta * 1000
+		velocity.x += SPEED
 		is_moving = true
 		anim.flip_h = false
+	else:
+		velocity.x = 0
 
 	# Jump
 	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS-1:
 		velocity.y = JUMP_VELOCITY
-		jump_count += 1
 		anim.play("jump")
+		jump_count += 1
 
 	# Variable jump height
 	if velocity.y < 0 and Input.is_action_just_released("jump"):
