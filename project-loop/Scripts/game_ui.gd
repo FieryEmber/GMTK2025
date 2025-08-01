0aad57572f7c9f2ec90075bbb40ee746e89187d0
Dessikret <lolyourdead229@gmail.com>
2025-07-31 22:15:01 -0400

    MapChanges & Added new physics layer for platform collisions
    new physics layer for platform collisions is layer 2 labeled ("platforms")

--- project-loop/Scripts/game_ui.gd
+++ project-loop/Scripts/game_ui.gd
@@ -5,5 +5,3 @@ extends Control
 @onready var left: Button = $MarginContainer/VBoxContainer2/HBoxContainer/Left
 @onready var down: Button = $MarginContainer/VBoxContainer2/HBoxContainer/Down
 @onready var right: Button = $MarginContainer/VBoxContainer2/HBoxContainer/Right
-@onready var timer: RichTextLabel = $MarginContainer/VBoxContainer/HBoxContainer/Timer
-@onready var progress_bar: ProgressBar = $MarginContainer/VBoxContainer/ProgressBar
