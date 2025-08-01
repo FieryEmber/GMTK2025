f42b140726c1929ed7447412bfb63e4b0f801c94
Hayden Galyean <haydengalyeanbiz@gmail.com>
2025-08-01 18:03:17 -0400

    staging before pull

--- project-loop/Scripts/mobius.gd
+++ project-loop/Scripts/mobius.gd
@@ -52,7 +52,7 @@ func _physics_process(delta):
 	else: 
 		velocity.x = 0
 	
-	if Input.is_action_pressed("down") and can_move_down:
+	if Input.is_action_pressed("down") and can_move_down and is_on_floor():
 		velocity.y += 50  
 		move_and_slide()  
 		
