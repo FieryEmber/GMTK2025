0c6c94a0cabb7c5006beb98b4fc365023dd8dbbd
Dessikret <lolyourdead229@gmail.com>
2025-08-02 23:26:00 -0400

    Greying out Ui

--- project-loop/Scripts/game_ui.gd
+++ project-loop/Scripts/game_ui.gd
@@ -7,3 +7,35 @@ extends Control
 @onready var right: Button = $MarginContainer/VBoxContainer2/HBoxContainer/Right
 
 @onready var mobius: CharacterBody2D = $"../../../Mobius"
+
+func _process(delta: float) -> void:
+
+	if mobius.can_move_right:
+		right.disabled = false
+	if not mobius.can_move_right:
+		right.disabled = true
+		pass
+
+	if mobius.can_move_left:
+		left.disabled = false
+	if not mobius.can_move_left:
+		left.disabled = true
+		pass
+
+	if mobius.can_move_down:
+		down.disabled = false
+	if not mobius.can_move_down:
+		down.disabled = true
+		pass
+
+	if mobius.can_jump:
+		up.disabled = false
+	if not mobius.can_jump:
+		up.disabled = true
+		pass
+
+	if mobius.can_teleport:
+		teleport.disabled = false
+	if not mobius.can_teleport:
+		teleport.disabled = true
+		pass
